a885fd2ee7e5f3c042bee990553b85b1aac67ef5
lauuu13 <40647513+lauuu13@users.noreply.github.com>
2026-08-16 13:57:12 +0800

    Delete plugins/capacitor-host-server directory

--- plugins/capacitor-host-server/src/index.js
+++
@@ -1,7 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-
-const HostServer = registerPlugin('HostServer', {
-  web: () => import('./web.js').then((m) => new m.HostServerWeb())
-});
-
-export default HostServer;
